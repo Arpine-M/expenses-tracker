26daabfbb8d799252aeba1f9e2cd53acd11e35fc
AM <malerischam@yahoo.com>
2025-01-15 01:51:11 +0400

    backend changes

--- backend/app.js
+++ backend/app.js
@@ -7,30 +7,25 @@ const userRouter = require("./routes/userRouter");
 const errorHandler = require("./middlewares/errorHandlerMiddleware");
 const categoryRouter = require("./routes/categoryRouter");
 const transactionRouter = require("./routes/transactionRouter");
-// Connect to MongoDB
-mongoose
-    .connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => console.log("Connected to MongoDB"))
-    .catch((err) => console.log(err));
+const connectDB = require("./utils/connectDB");
+
+connectDB();
 // Cors config
 const corsOptions = {
-    origin: ["http://localhost:5173"],
-    
-}
+  origin: ["http://localhost:5173"],
+};
 app.use(cors(corsOptions));
 
 // Middlewares
 app.use(express.json());
 
 // Routes
-app.use('/', userRouter);
-app.use('/', categoryRouter);
-app.use('/', transactionRouter);
-
+app.use("/", userRouter);
+app.use("/", categoryRouter);
+app.use("/", transactionRouter);
 
 // Error
 app.use(errorHandler);
-
 
 // Start the server
 const PORT = process.env.PORT || 8000;
